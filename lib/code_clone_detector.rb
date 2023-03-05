0fef8c6a2788921502ad6b1b6db30b8b19f54b38
MK-nn <okuken99@gmail.com>
2023-03-05 09:57:08 +0900

    update to say hello world

--- lib/code_clone_detector.rb
+++ lib/code_clone_detector.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "code_clone_detector/version"
+require_relative "code_clone_detector/hello"
 
 module CodeCloneDetector
   class Error < StandardError; end
---
+++ lib/code_clone_detector/hello.rb
@@ -0,0 +1,7 @@
+module CodeCloneDetector
+  class Hello
+    def self.hello
+      "Hello, world!"
+    end
+  end
+end
